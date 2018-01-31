4d7ccd696eb954b7399e3a754c7d0a476ec57740
Andrew Krall <ankwdf@mail.missouri.edu>
2018-01-30 20:32:36 -0600

    Added multiplication functionality

--- RelativeCalculator/app/src/main/java/com/example/dre/relativecalculator/Calculator.java
+++ RelativeCalculator/app/src/main/java/com/example/dre/relativecalculator/Calculator.java
@@ -10,6 +10,8 @@ public class Calculator extends AppCompatActivity {
     private TextView _screen;
     private String display = "";
     private Float operand;
+    private Float result = 1.0f;
+    private int loopCount = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,20 +41,46 @@ public class Calculator extends AppCompatActivity {
     }
 
     public void onClickOp (View v) {
+        int toggleMult = -1;
+        int toggleDiv = -1;
+        int toggleAdd = -1;
+        int toggleSub = -1;
+
         // No number has been entered
         if(display.isEmpty()) {
             return;
         }
         // Display is not empty and operand has not been set, set operand
-        if(!operand) {
-            operand = Float(display.toString());
+        if(operand == null) {
+            operand = Float.parseFloat(display.toString());
         }
-        else if(operand) {
+        else if(operand != null) {
             // Operand has been set, perform operation
+            //Parse through the display string to ensure that two numbers and an operand exist in the correct order.
+            //Check multiplication first.
+            System.out.println("\n\n\n\nGot here!!!");
+            String[] tokens = display.split("X|\\+");
+            for (String token : tokens) {
+                System.out.println(token);
+                result *= operand;
+                loopCount++;
+            }
+            if(loopCount % 2 == 0) {
+                System.out.println("\nGot in the if\n");
+                //This means two numbers have been entered, calculate the result and display it.
+                display = "";
+                display += result;
+                updateScreen();
+            }
+
         }
-        Button btn = (Button) v;
-        display += btn.getText();
-        updateScreen();
+
+
+            System.out.println("\nGot to else!\n");
+            Button btn = (Button) v;
+            display += btn.getText();
+            updateScreen();
+
     }
 
     public void onClickEquals (View v) {
