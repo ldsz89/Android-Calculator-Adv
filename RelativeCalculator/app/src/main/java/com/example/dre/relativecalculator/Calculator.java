6cf07896cb72915bd76d4a6c59bedbd90114edbc
Dre Shannon <ldsz89@mail.missouri.edu>
2018-01-30 19:28:44 -0600

    Working on onClickOp

--- RelativeCalculator/app/src/main/java/com/example/dre/relativecalculator/Calculator.java
+++ RelativeCalculator/app/src/main/java/com/example/dre/relativecalculator/Calculator.java
@@ -9,13 +9,16 @@ import android.widget.TextView;
 public class Calculator extends AppCompatActivity {
     private TextView _screen;
     private String display = "";
+    private Float operand;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
         _screen = (TextView)findViewById(R.id.textView);
+        display = "";
         _screen.setText(display);
+        operand = null;
     }
 
     protected void updateScreen() {
@@ -32,11 +35,27 @@ public class Calculator extends AppCompatActivity {
         Button btn = (Button) v;
         display = "";
         updateScreen();
+        operand = null;
     }
 
     public void onClickOp (View v) {
+        // No number has been entered
+        if(display.isEmpty()) {
+            return;
+        }
+        // Display is not empty and operand has not been set, set operand
+        if(!operand) {
+            operand = Float(display.toString());
+        }
+        else if(operand) {
+            // Operand has been set, perform operation
+        }
         Button btn = (Button) v;
         display += btn.getText();
         updateScreen();
+    }
+
+    public void onClickEquals (View v) {
+
     }
 }
